b28919f645242f28223c92718c6472f0dd66d6cf
Alex Alvarez <alexander-alvarez@github.com>
2017-11-20 22:45:39 -0500

    Add injection to user model

--- app/models/user.js
+++ app/models/user.js
@@ -1,5 +1,6 @@
 import DS from 'ember-data';
+import { inject } from '@ember/service';
 
 export default DS.Model.extend({
-
+  myService: inject()
 });
--- tests/unit/controllers/controller-with-model-test.js
+++ tests/unit/controllers/controller-with-model-test.js
@@ -1,18 +1,23 @@
 import { moduleFor, test } from 'ember-qunit';
 import { run } from '@ember/runloop';
+import engineResolverFor from 'ember-engines/test-support/engine-resolver-for';
+
+const resolver = engineResolverFor('my-engine');
+
 
 moduleFor('controller:controller-with-model', 'Unit | Controller | controller with model', {
-  // Specify the other units that are required for this test.
-  needs: ['model:user']
+  resolver,
+  needs: ['model:user', 'service:my-service']
 });
 
 // Replace this with your real tests.
 test('it exists', function(assert) {
-  const store = this.container.lookup('service:store');
-  
+
+ // const store = this.container.lookup('service:store');
+
   run(() => {
     let controller = this.subject({
-      store
+      //store
     });
     assert.ok(controller);
   });
